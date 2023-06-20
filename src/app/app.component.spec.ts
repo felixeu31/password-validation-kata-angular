d05f7628bb609a454119cc50aabc7d1c947b4787
Félix Díez Arias <fdiez@tunstall.es>
2023-06-20 17:50:58 +0200

    feat (passing test): should render save button

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import '@testing-library/jest-dom'
 
 function buildComponent() {
   return render(AppComponent, {
@@ -12,5 +13,10 @@ describe('Password validation kata', () => {
   it('should render password input', async () => {
     const { getByPlaceholderText } = await buildComponent();
     expect(getByPlaceholderText('Password')).toBeInTheDocument();
+  });
+
+  it('should render save button', async () => {
+    const { getByText } = await buildComponent();
+    expect(getByText('Save')).toBeInTheDocument();
   });
 });
