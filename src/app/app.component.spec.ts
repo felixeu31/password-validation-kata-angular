48c8d0bf671805f7f820b5f423351917aeaecfc2
Félix Díez Arias <fdiez@tunstall.es>
2023-06-21 17:10:48 +0200

    RED 'should print error message when password is shorter than 8 characters'

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/angular';
+import { render, fireEvent } from '@testing-library/angular';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import '@testing-library/jest-dom'
+import  userEvent  from '@testing-library/user-event'
 
 function buildComponent() {
   return render(AppComponent, {
@@ -18,5 +19,14 @@ describe('Password validation kata', () => {
   it('should render save button', async () => {
     const { getByText } = await buildComponent();
     expect(getByText('Save')).toBeInTheDocument();
+  });
+
+  it('should print error message when password is shorter than 8 characters', async () => {
+    const { getByText, getByPlaceholderText } = await buildComponent();
+
+    await userEvent.type(getByPlaceholderText('Password'), '1');
+    fireEvent.click(getByText('Save'));
+
+    expect(getByText('Password should not be shorter than 8 characters')).toBeInTheDocument();
   });
 });
