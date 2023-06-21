b972b481deae925a6aa912938056f235213c61f0
Félix Díez Arias <fdiez@tunstall.es>
2023-06-21 17:22:28 +0200

    REFACTOR wrap input and button inside a form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'password-validation-kata-angular';
+
+  form = this.formBuilder.group({
+    password: ['', []]
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+    
+  }
 }
